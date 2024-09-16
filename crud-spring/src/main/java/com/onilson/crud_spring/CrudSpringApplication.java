72541e1e3a3ddd2331637a72979d0f39cf82f986
onilson-csf <onilson.cardoso@gmail.com>
2024-09-16 15:37:15 -0300

    initial commit

---
+++ crud-spring/src/main/java/com/onilson/crud_spring/CrudSpringApplication.java
@@ -0,0 +1,29 @@
+package com.onilson.crud_spring;
+
+import com.onilson.crud_spring.model.Course;
+import com.onilson.crud_spring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
+}
